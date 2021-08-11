8ceea558ba7a1f529f3a48f877764112d56514a7
archinksagar <68829863+archinksagar@users.noreply.github.com>
2021-08-11 17:51:22 -0400

    first starting project

---
+++ reading.py
@@ -0,0 +1,18 @@
+# my_file = open(japan.txt,mode='w')
+# my_file()
+
+# with open('japan.txt',mode='w') as my_file:
+#     my_file.write('Hi are you there')
+
+# with open('japan.txt',mode='r') as my_file:
+#     print(my_file.read())
+
+from translate import Translator
+
+with open('japan.txt', mode='r') as my_file:
+    read = my_file.read()
+
+translator = Translator(to_lang='ja')
+translation = translator.translate((read))
+print(read)
+print(translation)
